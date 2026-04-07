3962d649c7b0c73b8db2d97542ca24322f7c438f
arinacoola <a.zvarunchyk@gmail.com>
2026-04-08 02:19:19 +0300

    множення в  GF(2^8)

---
+++ src/GF256.java
@@ -0,0 +1,15 @@
+public class GF256{
+    public static int mul2(int x){
+        x= x & 0xFF;
+        if((x & 0x80) != 0){
+            return ((x << 1) ^ 0x1b) & 0xFF;
+        }
+        else{
+            return (x << 1) & 0xFF;
+        }
+    }
+
+    public static int mul3(int x){
+        return (mul2(x) ^ x) & 0xFF;
+    }
+}
