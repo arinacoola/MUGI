d30203b3b0d33be4281a13b527a53298a79c7600
arinacoola <a.zvarunchyk@gmail.com>
2026-04-14 03:02:53 +0300

    encrypt

--- src/MugiCipher.java
+++ src/MugiCipher.java
@@ -1,2 +1,30 @@
 public class MugiCipher {
+    public static byte[] encryptData(byte[] input, byte[] key, byte[] iv){
+        if (input == null) {
+            throw new IllegalArgumentException("input must not be null");
+        }
+        MugiCore gen = new MugiCore();
+        gen.initCipher(key, iv);
+        byte[] res =new byte[input.length];
+        byte[] gBytes =new byte[8];
+        int blockCount = input.length/ 8;
+        int tail = input.length % 8;
+        for (int block = 0;block < blockCount;block++) {
+            long gamma = gen.nextBlock();
+            ByteUtils.longToBytes(gamma,gBytes,0);
+            int start = block*8;
+            for (int i = 0;i < 8; i++) {
+                res[start + i]=(byte) (input[start + i] ^ gBytes[i]);
+            }
+        }
+        if (tail > 0){
+            long gamma = gen.nextBlock();
+            ByteUtils.longToBytes(gamma, gBytes, 0);
+            int start = blockCount * 8;
+            for (int i = 0;i < tail; i++) {
+                res[start + i] = (byte) (input[start +i] ^ gBytes[i]);
+            }
+        }
+        return res;
+    }
 }
